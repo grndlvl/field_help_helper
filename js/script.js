1d4ff315fc7cd12181760d2e0075893941f7d6e8
Jonathan DeLaigle <delaigle.jonathan@gmail.com>
2016-03-09 15:16:24 -0500

    Some minor js changes

--- js/script.js
+++ js/script.js
@@ -3,24 +3,16 @@
  * JavaScript file for the Coffee module.
  */
 
-(function ($, Drupal, drupalSettings, context) {
+(function ($, Drupal, drupalSettings) {
+
+  'use strict';
 
   Drupal.behaviors.field_help_helper = {
-    attach: function () {
-      var $formWrapper = $('.form-wrapper', context);
-      var $helperLink = $('a[data-drupal-selector="edit-field-help-helper-link"]', context);
-      var $description = $('.description', context);
-
-      $formWrapper.each(function(){
-        if ($(this).find($description).length) {
-          $(this)
-            .find($helperLink)
-            .on('mouseover', function(){
-              $(this).parent($formWrapper).find($description).addClass('hovered');
-            }).on('mouseout', function(){
-              $(this).parent($formWrapper).find($description).removeClass('hovered');
-            });
-        }
+    attach: function (context) {
+      $('a[data-drupal-selector="edit-field-help-helper-link"]', context).each(function () {
+        $(this).on('mouseover mouseout', function() {
+          $('.description', $(this).parent()).toggleClass('hovered');
+        });
       });
     }
   }
