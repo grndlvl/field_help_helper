879291898d9b84b5464d6af723dbd5fb124d5f7a
Jonathan DeLaigle <delaigle.jonathan@gmail.com>
2016-03-10 15:20:23 -0500

    Minor modifications for JS

--- js/script.js
+++ js/script.js
@@ -1,23 +1,3 @@
-/**
- * @file
- * JavaScript file for the Coffee module.
- */
-
-(function ($, Drupal, drupalSettings) {
-
-  'use strict';
-
-  Drupal.behaviors.field_help_helper = {
-    attach: function (context) {
-      var $edit_links = $('a[data-drupal-selector="edit-field-help-helper-link"]', context);
-      var help_text_selector = '.description';
-
-      $edit_links.each(function () {
         $(this)
-          .once()
-          .appendTo(help_text_selector)
-      });
-    }
-  }
-
-}(jQuery, Drupal, drupalSettings));
+          .once('field-help-helper-link')
+          .appendTo($(help_text_selector, $(this).parent()))
