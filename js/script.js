52d0f739a8af22f311b19b12b8e39cc8ea42bbd6
Jonathan DeLaigle <delaigle.jonathan@gmail.com>
2016-03-10 15:26:14 -0500

    Fixing JS take 2

--- js/script.js
+++ js/script.js
@@ -1,3 +1,24 @@
+/**
+ * @file
+ * JavaScript file for the Field help helper module.
+ */
+
+(function ($, Drupal, drupalSettings) {
+
+  'use strict';
+
+  Drupal.behaviors.field_help_helper = {
+    attach: function (context) {
+      var $edit_links = $('a[data-drupal-selector="edit-field-help-helper-link"]', context);
+      var help_text_selector = '.description';
+
+      $edit_links.each(function () {
         $(this)
           .once('field-help-helper-link')
           .appendTo($(help_text_selector, $(this).parent()))
+      });
+    }
+  }
+
+}(jQuery, Drupal, drupalSettings));
+
