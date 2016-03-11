83d75d48fb60abcfa39f4234f46ec93e32105b26
Jonathan DeLaigle <delaigle.jonathan@gmail.com>
2016-03-11 11:20:05 -0500

    Adding detection for the help field to be visible prior to attempting to move the link next to the help text

--- js/script.js
+++ js/script.js
@@ -10,12 +10,16 @@
   Drupal.behaviors.field_help_helper = {
     attach: function (context) {
       var $edit_links = $('a[data-drupal-selector="edit-field-help-helper-link"]', context);
+      var $help_text;
       var help_text_selector = '.description';
 
       $edit_links.each(function () {
-        $(this)
-          .once('field-help-helper-link')
-          .appendTo($(this).parent().find(help_text_selector));
+        $help_text = $(this).parent().find(help_text_selector).is(':visible');
+        if ($help_text.length > 0) {
+          $(this)
+            .once('field-help-helper-link')
+            .appendTo($(this).parent().find(help_text_selector));
+        }
       });
     }
   }
